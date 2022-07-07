1cca86f7268d9e71728e6b5f4232beb7f2ffb700
nreis11 <nicholasreis@icloud.com>
2022-07-07 13:28:28 -0700

    Add hero bg slides

--- js/main.js
+++ js/main.js
@@ -174,24 +174,24 @@
   var sliderMain = function () {
     $("#hero .flexslider").flexslider({
       animation: "fade",
-      slideshowSpeed: 5000,
+      slideshowSpeed: 7000,
       directionNav: true,
-      start: function () {
-        setTimeout(function () {
-          $(".slider-text").removeClass("animated fadeInUp");
-          $(".flex-active-slide")
-            .find(".slider-text")
-            .addClass("animated fadeInUp");
-        }, 500);
-      },
-      before: function () {
-        setTimeout(function () {
-          $(".slider-text").removeClass("animated fadeInUp");
-          $(".flex-active-slide")
-            .find(".slider-text")
-            .addClass("animated fadeInUp");
-        }, 500);
-      },
+      // start: function () {
+      //   setTimeout(function () {
+      //     $(".slider-text").removeClass("animated fadeInUp");
+      //     $(".flex-active-slide")
+      //       .find(".slider-text")
+      //       .addClass("animated fadeInUp");
+      //   }, 500);
+      // },
+      // before: function () {
+      //   setTimeout(function () {
+      //     $(".slider-text").removeClass("animated fadeInUp");
+      //     $(".flex-active-slide")
+      //       .find(".slider-text")
+      //       .addClass("animated fadeInUp");
+      //   }, 500);
+      // },
     });
   };
 
